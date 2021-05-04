43a7cc738af98e0c2dcf9fcae5b1da5bdce76c71
meganukebmp <kristiana2000@abv.bg>
2021-05-05 00:48:43 +0300

    So much bloat

--- src/Icons.java
+++ src/Icons.java
@@ -3,6 +3,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public interface Icons {
-    ImageIcon MARIO = new ImageIcon(Icons.class.getResource("/mario.gif"));
-    ImageIcon SHELL = new ImageIcon(Icons.class.getResource("/shell.gif"));
+    ImageIcon DRAGON = new ImageIcon(Icons.class.getResource("/dragon.gif"));
 }
